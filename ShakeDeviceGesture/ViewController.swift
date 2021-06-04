f62c09fed5587788fc31db9ab0d81e12c521e000
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-06-04 15:56:54 +0300

    init

--- ShakeDeviceGesture/ViewController.swift
+++ ShakeDeviceGesture/ViewController.swift
@@ -11,9 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+        becomeFirstResponder()
     }
-
-
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            let alert = UIAlertController(title: "Shake detected", message: "User got mad and shook phone", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+            present(alert, animated: true)
+        }
+    }
 }
 
